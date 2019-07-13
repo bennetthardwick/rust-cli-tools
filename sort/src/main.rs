d8982e063cd6ae8c50d08e7ea751b72bd20c5f31
Bennett Hardwick <me@bennetthardwick.com>
2019-07-13 22:23:41 +1000

    Update sort

--- sort/src/main.rs
+++ sort/src/main.rs
@@ -1,34 +1,12 @@
 use itertools::sorted;
-use std::io::{self, Stdin, Write};
-
-struct Reader {
-    stdin: Stdin
-}
-
-impl Iterator for Reader {
-    type Item = String;
-    fn next(&mut self) -> Option<String> {
-        let mut data = String::new();
-        match self.stdin.read_line(&mut data) {
-            Ok(size) => {
-                if size == 0 {
-                    return None
-                } else {
-                    return Some(data);
-                }
-            }
-            _ => return None
-        }
-    }
-}
+use std::io::{self, BufRead, Write};
 
 fn main() {
-    let r = Reader { stdin: io::stdin() };
+    let stdin = io::stdin();
     let stdout = io::stdout();
     let mut handle = stdout.lock();
-
-    for l in sorted(r) {
+    let lines = stdin.lock().lines().map(|l| l.unwrap());
+    for l in sorted(lines) {
         write!(handle, "{}", l).unwrap();
     }
-
 }
